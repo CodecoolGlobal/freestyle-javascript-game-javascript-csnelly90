4e6473e590b7086e87e0422a1ceefd9693d86d32
csnelly90 <csnelly90@gmail.com>
2022-02-16 03:57:30 +0100

    add password handling functions

--- server.py
+++ server.py
@@ -24,5 +24,18 @@ def leaderboard():
     return render_template("leaderboard.html")
 
 
+# Password handling functions
+
+
+def hash_password(plain_text_password):
+    hashed_bytes = bcrypt.hashpw(plain_text_password.encode("utf-8"), bcrypt.gensalt())
+    return hashed_bytes.decode("utf-8")
+
+
+def verify_password(plain_text_password, hashed_password):
+    hashed_bytes_password = hashed_password.encode("utf-8")
+    return bcrypt.checkpw(plain_text_password.encode("utf-8"), hashed_bytes_password)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
